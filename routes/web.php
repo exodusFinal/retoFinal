ee738a74fc55fa41084dcdf89dc05a6214d818b1
Julen Prieto <prietojulen@gmail.com>
2020-02-19 13:50:52 +0100

    Index prueba

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//esto es una prueba para ver el header
+Route::get('/index', function () {
+    return view('index');
+});
