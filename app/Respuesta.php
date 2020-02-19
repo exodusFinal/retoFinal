7be6eb853550ee1796043495acc90be9c9626f3a
aritz <aritz.martinezh@gmail.com>
2020-02-19 11:48:50 +0100

    todo listo para empezar

---
+++ app/Respuesta.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Respuesta extends Model
+{
+    public function pregunta()
+    {
+        return $this->belongsTo('App\Pregunta');
+    }
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('descripcion');
+            $table->integer('puntosUsu');
+            $table->string('foto');
             $table->rememberToken();
             $table->timestamps();
         });
