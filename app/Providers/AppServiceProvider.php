090aedaeee0da349017d7f99a42f432696f2fa00
AritzMartinez <aritz.martinezh@gmail.com>
2020-02-26 11:54:24 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(200);
+    Schema::defaultStringLength(180);    }
     }
-}
