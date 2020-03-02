65d6fa993c876df75cddd49eb56ac64943594265
Unknown <aritz.martinezh@gmail.com>
2020-03-02 08:23:03 +0100

    Filtro por tema y por titulo

--- app/Pregunta.php
+++ app/Pregunta.php
@@ -26,9 +26,10 @@ class Pregunta extends Model
     public function scopeTitulo($query,$titulo){
         if($titulo!=null)
 
-            return $query->Where('titulo','=', "$titulo");
+            return $query->Where('titulo','LIKE', "%$titulo%");
 
     }
+
     public function scopeTema_id($query,$tema_id){
         if($tema_id!=null)
 
